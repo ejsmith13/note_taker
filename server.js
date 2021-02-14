dcb2ed19e66ccf59b96bdbe77073a24e6ea14421
ejsmith13 <ejsmithwork@gmail.com>
2021-02-14 11:03:41 -0500

    linking notes page

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //send user to initial home screen
-// Basic route that sends the user first to the AJAX Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+//route that sends user to notes taking page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 
 
